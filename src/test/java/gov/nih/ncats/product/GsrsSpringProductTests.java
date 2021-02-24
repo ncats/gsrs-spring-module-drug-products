389308d42334e956079f6f62d567fbe52496958d
Newatia <Archana.Newatia@fda.gov>
2021-02-24 15:36:35 -0500

    removed .iml files

---
+++ src/test/java/gov/nih/ncats/product/GsrsSpringProductTests.java
@@ -0,0 +1,13 @@
+package gov.nih.ncats.product;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+class GsrsSpringProductTests {
+
+  //  @Test
+    void contextLoads() {
+    }
+
+}
