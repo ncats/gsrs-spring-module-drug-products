ecd024ed66480b31a3ed46f453a02d9936737774
Tyler Peryea <tylerperyea@gmail.com>
2022-03-05 11:46:15 -0500

    Update ProductComponent.java

--- gsrs-module-products-spring-boot-autoconfigure/src/main/java/gov/hhs/gsrs/products/product/models/ProductComponent.java
+++ gsrs-module-products-spring-boot-autoconfigure/src/main/java/gov/hhs/gsrs/products/product/models/ProductComponent.java
@@ -93,7 +93,7 @@ public class ProductComponent extends ProductCommonData {
     @EqualsAndHashCode.Exclude
     @JsonIgnore
     @ManyToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name="PRODUCT_ID", referencedColumnName = "PRODUCTID")
+    @JoinColumn(name="PRODUCT_ID", referencedColumnName = "PRODUCT_ID")
     public Product owner;
 
     public void setOwner(Product product) {
