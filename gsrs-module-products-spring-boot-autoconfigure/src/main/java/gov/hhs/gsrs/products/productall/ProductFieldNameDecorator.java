9fe8bd5e5776b4a3027e2884ed51db56b6fa4e36
Newatia <Archana.Newatia@fda.gov>
2022-07-08 00:41:34 -0400

    updated Field Name Decorator

--- gsrs-module-products-spring-boot-autoconfigure/src/main/java/gov/hhs/gsrs/products/productall/ProductFieldNameDecorator.java
+++ gsrs-module-products-spring-boot-autoconfigure/src/main/java/gov/hhs/gsrs/products/productall/ProductFieldNameDecorator.java
@@ -5,38 +5,54 @@ import ix.core.FieldNameDecorator;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ProductFieldNameDecorator implements FieldNameDecorator{
+public class ProductFieldNameDecorator implements FieldNameDecorator {
 
-	private static final Map<String,String> displayNames;
+    private static final Map<String, String> displayNames;
 
-	static{
-			Map<String, String> m = new HashMap<>();
+    static {
+        Map<String, String> m = new HashMap<>();
 
-		    m.put("root_productNDC" , "Product NDC");
-			m.put("root_productType" , "Product Type");
-		    m.put("root_status" , "Status");
-		    m.put("root_nonProprietaryName", "Non Proprietary Name");
-		    m.put("root_productCompanyAllList_labelerName" , "Labeler Name");
-		    m.put("root_productNameAllList_productName" , "Product Name");
-		    m.put("root_sponsorName" , "Sponsor Name");
-		    m.put("Ingredient Name" , "Ingredient Name");
-		    m.put("root_appTypeNumber", "Application Type Number");
-		    m.put("root_productIngredientAllList_dosageFormName" , "Dosage Form Name");
-			m.put("root_productCompanyAllList_countryWithoutCode" , "Country");
-		    m.put("root_productIngredientAllList_ingredientType" , "Ingredient Type");
-		    m.put("root_productIngredientAllList_activeMoietyName", "Active Moiety Name");
-            m.put("root_productIngredientAllList_substanceName", "Ingredient Name");
+        m.put("root_productNDC", "Product NDC");
+        m.put("root_nonProprietaryName", "Non Proprietary Name");
+        m.put("root_productType", "Product Type");
+        m.put("root_status", "Status");
+        m.put("root_marketingCategoryName", "Marketing Category Name");
+        m.put("root_isListed", "Is Listed");
+        m.put("root_routeName", "Route of Administration");
+        m.put("root_appTypeNumber", "Application Type Number");
+        m.put("root_appType", "Application Type");
+        m.put("root_appNumber", "Application Number");
+        m.put("root_sponsorName", "Sponsor Name");
+        m.put("root_unitPresentation", "Unit of Presentation");
+        m.put("root_source", "Source");
+        m.put("root_sourceType", "Source Type");
+        m.put("root_fromTable", "From Table");
+        m.put("root_provenance", "Provenance");
 
-		    displayNames = m;
-	}
+        m.put("root_productNameAllList_productName", "Product Name");
 
-	@Override
-	public String getDisplayName(String field) {
-		String fdisp = displayNames.get(field);
-		if (fdisp == null) {
-			return field;
-		}
-		return fdisp;
-	}
+        m.put("root_productCompanyAllList_labelerName", "Labeler Name");
+        m.put("root_productCompanyAllList_address", "Labeler Address");
+        m.put("root_productCompanyAllList_zip", "Labeler Zipcode");
+        m.put("root_productCompanyAllList_countryWithoutCode", "Country");
+        m.put("root_productCompanyAllList_labelerDuns", "Labeler Duns Number");
+
+        m.put("root_productIngredientAllList_activeMoietyName", "Active Moiety Name");
+        m.put("root_productIngredientAllList_substanceName", "Ingredient Name");
+        m.put("Ingredient Name", "Ingredient Name");
+        m.put("root_productIngredientAllList_ingredientType", "Ingredient Type");
+        m.put("root_productIngredientAllList_dosageFormName", "Dosage Form Name");
+
+        displayNames = m;
+    }
+
+    @Override
+    public String getDisplayName(String field) {
+        String fdisp = displayNames.get(field);
+        if (fdisp == null) {
+            return field;
+        }
+        return fdisp;
+    }
 
 }
