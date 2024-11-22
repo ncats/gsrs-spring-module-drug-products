35ae5e5e76e0362aa31fec1b499b679e55d09879
Mitch Miller <mitch.miller@thinkscience.us>
2024-11-22 10:00:43 -0500

    made test work on non-windows platforms

--- gsrs-module-products-spring-boot-autoconfigure/src/test/java/gov/hhs/gsrs/products/DataDirMonitorTaskTests.java
+++ gsrs-module-products-spring-boot-autoconfigure/src/test/java/gov/hhs/gsrs/products/DataDirMonitorTaskTests.java
@@ -2,6 +2,7 @@ package gov.hhs.gsrs.products;
 
 import gov.hhs.gsrs.products.product.models.Product;
 import gov.hhs.gsrs.products.product.tasks.DataDirMonitorTask;
+import gov.hhs.gsrs.products.product.utils.ShellCommandRunner;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
@@ -13,21 +14,12 @@ public class DataDirMonitorTaskTests {
 
     @Test
     void getFileNameTest() {
-        String fullFilePath = "D:\\temp\\product_data\\23dbde51-ded9-505e-e063-6294a90a46aa.xml";
+        String fullFilePath = ShellCommandRunner.isWindows()
+                ? "D:\\temp\\product_data\\23dbde51-ded9-505e-e063-6294a90a46aa.xml"
+                : "/tmp/product_data/23dbde51-ded9-505e-e063-6294a90a46aa.xml";
         String expected = "23dbde51-ded9-505e-e063-6294a90a46aa.xml";
         String fileName= DataDirMonitorTask.getFileName(fullFilePath);
         Assertions.assertEquals(expected, fileName);
-    }
-
-    @Test
-    void testSubstitution() {
-        String fullFilePath = "D:\\temp\\product_data\\23dbde51-ded9-505e-e063-6294a90a46aa.xml";
-        String result = fullFilePath.replace("\\", "\\\\");
-        int totalBackSlash = 0;
-        for(char c : result.toCharArray() ) {
-            if(c == '\\') totalBackSlash++;
-        }
-        Assertions.assertEquals(6, totalBackSlash);
     }
 
     @Test
